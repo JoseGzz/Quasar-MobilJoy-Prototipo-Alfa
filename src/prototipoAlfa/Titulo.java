ea565c9ddb61adcdc77693125790bbdf58fa2b6e
Fred Roblero <realdazzlingblaze@gmail.com>
2015-04-07 20:56:56 -0500

    Agrega las imagenes del juego. Agrega el boton saltar. Agrega el fondo.

--- src/prototipoAlfa/Titulo.java
+++ src/prototipoAlfa/Titulo.java
@@ -8,6 +8,10 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.net.URL;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -17,35 +21,63 @@ import javax.swing.JPanel;
  *
  * @author José (el mero mero sabor ranshero) :V
  */
-public class Titulo extends JFrame {
+public class Titulo extends JFrame implements MouseListener {
     
    
     private Image imgPrincipal; // imagen del conejo
-    private Image imgBack; // imagen del fondo
+    private Image imgSaltar; // imagen del fondo
     JPanel panel = new JPanel(); // panel para insertar el boton y otros elementos
     JButton button = new JButton("Saltar >>"); // boton
+    Image imgBotonSaltar;
+    Image imgFondo;
+    Boton btnBotonSaltar;
 
     public Titulo (String strTitulo) {
+
         // se cargan las imagenes
-        URL urlImagenConejo = this.getClass().getResource("principal.gif");
-        URL urlBack = this.getClass().getResource("whiteBack.jpg");
-        imgPrincipal = Toolkit.getDefaultToolkit().getImage(urlImagenConejo);
-        imgBack = Toolkit.getDefaultToolkit().getImage(urlBack);
+        URL urlFondo = this.getClass().getResource("background1.jpg");
+        imgFondo = Toolkit.getDefaultToolkit().getImage(urlFondo);
+        
+        URL urlBotonSaltar = this.getClass().getResource("btn_saltar.png");
+        imgBotonSaltar = Toolkit.getDefaultToolkit().getImage(urlBotonSaltar);
+        
+        btnBotonSaltar = new Boton(0 , 0 /*this.getWidth() - imgBotonSaltar.getWidth(this),
+           this.getHeight() - imgBotonSaltar.getHeight(this)*/ , getWidth(), 
+                getHeight(),  Toolkit.getDefaultToolkit().getImage(urlBotonSaltar));
+
         // el acomodo de los elementos en el panel es libre
-        panel.setLayout(null);
+       // panel.setLayout(null);
         // configuracion del boton
-        button.setBounds(400, 400, 100, 100);
-        panel.add(button); // se aniade el boton al panel
-        add(panel); // se aniade el panel al frame
+       // button.setBounds(400, 400, 100, 100);
+       // panel.add(button); // se aniade el boton al panel
+       //  add(panel); // se aniade el panel al frame
         // configuracion del frame
         setVisible(true);
-        setSize(500, 500);
+        setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle(strTitulo);
         // se enlaza al boton el listener correspondiente
         button.addActionListener(new SiguienteFrame());
+        addMouseListener(this);
     }
-    
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+
+ 
     public class SiguienteFrame  implements ActionListener {
         // cuando se presione el boton
         public void actionPerformed(ActionEvent ev) {
@@ -72,7 +104,30 @@ public class Titulo extends JFrame {
 
     public void paint(Graphics graDibujo) {
           // graDibujo.drawImage(imgBack, 0, 0, this); // se dibuja un fondo blanco
-           graDibujo.drawImage(imgPrincipal, 200, 200, this); // imagen del conejo
+        
+       //   graDibujo.drawImage(imgFondo, 0, 0, this); // imagen del conejo
+          btnBotonSaltar.paint(graDibujo, this); 
+          graDibujo.drawImage(imgBotonSaltar, this.getWidth() - imgBotonSaltar.getWidth(this),
+           this.getHeight() - imgBotonSaltar.getHeight(this), this);
+          
            
+    }
+    
+    
+       
+    public void mousePressed(MouseEvent mseEvent) { 
+        //Si hay interseccion entre el planeta y el mouse
+        if ( btnBotonSaltar.intersecta(mseEvent.getX(), mseEvent.getY())) {
+            System.out.println("si jaló");
+// se abre una segunda vetana
+            new secondFrame("Second window");
+            // se cierra el frame presente
+            Titulo.this.setVisible(false);
+            Titulo.this.dispose();           
+
+// bClick = true; //prendo la booleana
+        
+        }
+       
     }
 }
